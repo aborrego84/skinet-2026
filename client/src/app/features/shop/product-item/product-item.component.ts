fcb52e442cf272d88d88988e805afb391270a5e7
Angel Borrego <angel.borrego@gmail.com>
2026-06-21 01:49:00 -0400

    End Section 10 OK

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -3,6 +3,7 @@ import { Product } from '../../../shared/models/product';
 import { MatCard, MatCardContent, MatCardActions } from '@angular/material/card';
 import { MatAnchor } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
@@ -11,7 +12,8 @@ import { MatIcon } from "@angular/material/icon";
     MatCardContent,
     MatCardActions,
     MatAnchor,
-    MatIcon
+    MatIcon,
+    RouterLink
 ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
