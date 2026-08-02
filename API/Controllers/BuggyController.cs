80fee9a2f8dfc8e53fa031ce66b76cc7fa1da732
Angel Borrego <angel.borrego@gmail.com>
2026-08-01 22:13:06 -0400

    End of Section 14

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -34,6 +36,14 @@ namespace API.Controllers
         public IActionResult GetValidationError(CreateProductDTO product)
         {
             return Ok();
+        }
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Ok("Hello " + name + " with the id of " + id);
         }
     }
 }
